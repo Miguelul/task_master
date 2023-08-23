301c64e6ac422b121c4d28393bcef8181ed0b8a0
Miguelul <mguelantonio00@gmail.com>
2023-08-23 09:59:22 -0400

    FirebaseOptions

--- lib/utils/Constants.dart
+++ lib/utils/Constants.dart
@@ -2,10 +2,8 @@ const String isLogin = 'isLogin';
 const String isLoginType = 'isLoginType';
 const String apiKey = "ba473d2522924658b172ec25772ee72c";
 
-
 const String taskChannelId = "10001";
 const String taskChannelName = "Task Reminder";
 const String taskChannelDescription = "Task reminder description";
-
-
-
+const String projectId = 'task-app-3001e';
+const String databaseURL = 'https://task-app-3001e.firebaseio.com';
